728c1076f186078e4137c6f1ee514b6bfeb1c756
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2020-09-15 21:11:28 +0100

    improve rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,67 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+
+const input = "./src/Cmi5.ts";
 
 const extensions = [
   ".js",
   ".ts"
 ];
 
-export default {
-  input: "./src/Cmi5.ts",
+const resolveOptions = {
+  extensions: extensions
+};
+
+const babelPluginOptions = {
+  babelHelpers: "bundled",
+  extensions: extensions
+};
+
+export default [{
+  input: input,
   plugins: [
     resolve({
-      extensions: extensions
+      ...resolveOptions,
+      browser: true
     }),
-    commonjs(),
-    babel({
-      babelHelpers: "bundled",
-      extensions: extensions
-    })
+    commonjs(), // Used for Axios import
+    babel(babelPluginOptions),
+    terser()
   ],
   output: [
     {
-      file: pkg.main,
+      file: pkg.browser,
       format: "umd",
       name: "Cmi5"
     },
     {
       file: pkg.module,
-      format: "es"
+      format: "esm",
+      exports: "default"
     }
   ]
-};
+}, {
+  input: input,
+  plugins: [
+    resolve({
+      ...resolveOptions,
+      browser: false
+    }),
+    commonjs(), // Used for Axios import
+    json(),
+    babel(babelPluginOptions),
+    terser()
+  ],
+  external: ["http", "https", "url", "zlib", "stream", "assert", "tty", "util", "os", "debug", "follow-redirects", "supports-color", "ms", "has-flag"],
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "default"
+    }
+  ]
+}];
