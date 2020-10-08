3ceb04c0638c59086ffe2a9395ff45711b3db956
larry kirschner <larrykirschner@gmail.com>
2020-10-08 13:29:25 -0700

    moves jest config to its own file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { defaults } = require("jest-config");
+module.exports = {
+  testURL: "http://example.com",
+  transform: {
+    ".(ts|tsx)": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "js"],
+};
