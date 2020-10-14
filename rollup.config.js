75dee25fa25e58852338ff9edcd94391061b04cd
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2020-10-14 17:47:19 +0100

    removed cjs export (#24)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 
@@ -39,42 +38,6 @@ export default [
       {
         file: pkg.module,
         format: "esm",
-        exports: "default",
-      },
-    ],
-  },
-  {
-    input: input,
-    plugins: [
-      resolve({
-        ...resolveOptions,
-        browser: false,
-      }),
-      commonjs(), // Used for Axios import
-      json(),
-      babel(babelPluginOptions),
-      terser(),
-    ],
-    external: [
-      "http",
-      "https",
-      "url",
-      "zlib",
-      "stream",
-      "assert",
-      "tty",
-      "util",
-      "os",
-      "debug",
-      "follow-redirects",
-      "supports-color",
-      "ms",
-      "has-flag",
-    ],
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
         exports: "default",
       },
     ],
