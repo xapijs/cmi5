2209c970a06d468b07cd5351c27533c690d12a00
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2021-09-07 17:40:53 +0100

    fix broken build with axios update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 
@@ -26,6 +27,7 @@ export default [
         browser: true,
       }),
       commonjs(), // Used for Axios import
+      json(),
       babel(babelPluginOptions),
       terser(),
     ],
